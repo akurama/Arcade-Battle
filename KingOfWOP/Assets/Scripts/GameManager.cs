c36cec25a5988e3d1d8f3d437ccc3b0d980d1611
AkuramaLP <jonathan.witt1998@gmail.com>
2017-12-04 22:37:23 +0100

    Add Existing project

---
+++ KingOfWOP/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GamemodeBase> Gamemodes;
+    public List<GamemodeBase> randomGamemodes = new List<GamemodeBase>();
+
+    public List<TextMeshProUGUI> texts;
+
+    public int multiplyer = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        RandomOrder();
+        multiplyer = 1;
+        PlayAnim();
+        FirstGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayAnim()
+    {
+        for (int i = 0; i < texts.Count; i++)
+        {
+            texts[i].text = randomGamemodes[i].gameObject.name;
+        }
+    }
+
+    void FirstGame()
+    {
+        randomGamemodes[0].gameObject.SetActive(true);
+    }
+
+    void RandomOrder()
+    {
+        List<GamemodeBase> store = Gamemodes;
+
+        int i = store.Count;
+
+        for (int j = 0; j < i; j++)
+        {
+            int index = Random.Range(0, store.Count);
+
+            randomGamemodes.Add(store[index]);
+            store.Remove(store[index]);
+        }
+    }
+
+    public void Test()
+    {
+        
+    }
+}
