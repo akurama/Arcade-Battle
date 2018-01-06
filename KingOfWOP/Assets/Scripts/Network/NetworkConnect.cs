37a8c6713137c96b86daf720af6156c946dd2ff5
Jonathan W <jonathan.witt1998@gmail.com>
2018-01-06 19:24:37 +0100

    Make Sweet Singelton

--- KingOfWOP/Assets/Scripts/Network/NetworkConnect.cs
+++ KingOfWOP/Assets/Scripts/Network/NetworkConnect.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NetworkConnect : MonoBehaviour 
 {
 
+	private string playerName;
+	public static NetworkConnect instance;
+
 	void Awake()
 	{
-		
+		SetNickname();
+		MakeSingleton();
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+
 	}
 	
 	// Update is called once per frame
@@ -29,7 +34,25 @@ public class NetworkConnect : MonoBehaviour
 
 	private void SetNickname()
 	{
-		
+		playerName = PlayerPrefs.GetString("PlayerName") + "#" + Random.Range(1000, 9999);
+	}
+
+	private void MakeSingleton()
+	{
+		if(instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else if(instance != this)
+		{
+			Destroy(this);
+		}
+	}
+
+	public void ReloadScene()
+	{
+		SceneManager.LoadScene("OnlineLobby");
 	}
 
 }
