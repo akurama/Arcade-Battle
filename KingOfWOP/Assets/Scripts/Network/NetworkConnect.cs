05a626445132d4b5166a7169a357c694ab99697b
Jonathan W <jonathan.witt1998@gmail.com>
2018-01-06 23:13:26 +0100

    Make Cool Networklobby Stuff

--- KingOfWOP/Assets/Scripts/Network/NetworkConnect.cs
+++ KingOfWOP/Assets/Scripts/Network/NetworkConnect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class NetworkConnect : MonoBehaviour 
 {
@@ -9,16 +10,31 @@ public class NetworkConnect : MonoBehaviour
 	private string playerName;
 	public static NetworkConnect instance;
 
+	public GameObject mainCanvas;
+
+	//PlayerCount
+	public GameObject playerCountText;
+	public bool refreshPlayerCountText;
+
+	//CreateRoomStuff
+	public GameObject roomCreateCanvas;
+	public GameObject roomNameText;
+
+	//RoomCanvas
+	public GameObject roomCanvas;
+
 	void Awake()
 	{
 		SetNickname();
-		MakeSingleton();
+		MakeSingleton();		
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		InitUI();
+		Debug.Log("Connecting to server.......");
+		PhotonNetwork.ConnectUsingSettings("0.0.1");	
 	}
 	
 	// Update is called once per frame
@@ -27,9 +43,107 @@ public class NetworkConnect : MonoBehaviour
 		
 	}
 
+	void OnDestroy()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	//PhotonCallbacks
 	private void OnConnectedToMaster()
 	{
-		
+		Debug.Log("Connection succeded");
+		PhotonNetwork.playerName = playerName;
+		StartCoroutine("RefreshPlayerCount");
+		PhotonNetwork.JoinLobby(TypedLobby.Default);
+	}
+
+	private void OnJoinedLobby()
+	{
+		Debug.Log("Joined Lobby.....");
+	}
+
+	private void OnJoinedRoom()
+	{
+		Debug.Log("Joined Room");
+		OpenRoomCanvas();
+	}
+
+	private void OnLeftRoom()
+	{
+		Debug.Log("Room Left");
+		CloseRoomCanvas();
+	}
+
+	private void OnDisconnectedFromPhoton()
+	{
+		instance = null;
+		Destroy(this.gameObject);
+	}
+	//END Callbacks
+
+	public void CreateRoom()
+	{
+		if(!PhotonNetwork.inRoom)
+		{
+			string roomName = roomNameText.GetComponent<TMP_InputField>().text;
+
+			RoomOptions roomOptions = new RoomOptions();
+			roomOptions.MaxPlayers = 2;
+
+			TypedLobby typedLobby = new TypedLobby();
+			typedLobby.Type = LobbyType.Default;
+
+			PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby);
+		}
+	}
+
+	public void JoinRoom()
+	{
+		string roomName = "";
+		PhotonNetwork.JoinRoom(roomName);
+	}
+
+	public void LeaveRoom()
+	{
+		PhotonNetwork.LeaveRoom();
+	}
+
+	public void JoinRandomRoom()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	public void OpenCreateRoomCanvas()
+	{
+		roomCreateCanvas.SetActive(true);
+		mainCanvas.SetActive(false);
+		roomCanvas.SetActive(false);
+	}
+
+	public void CloseCreateRoomCanvas()
+	{
+		mainCanvas.SetActive(true);
+		roomCreateCanvas.SetActive(false);
+		roomCanvas.SetActive(false);
+	}
+
+	public void OpenRoomCanvas()
+	{
+		roomCreateCanvas.SetActive(false);
+		mainCanvas.SetActive(false);
+		roomCanvas.SetActive(true);
+	}
+
+	public void CloseRoomCanvas()
+	{
+		roomCreateCanvas.SetActive(false);
+		mainCanvas.SetActive(true);
+		roomCanvas.SetActive(false);
+	}
+
+	public void Disconect()
+	{
+		PhotonNetwork.Disconnect();
 	}
 
 	private void SetNickname()
@@ -53,6 +167,26 @@ public class NetworkConnect : MonoBehaviour
 	public void ReloadScene()
 	{
 		SceneManager.LoadScene("OnlineLobby");
+	}
+
+	IEnumerator RefreshPlayerCount()
+	{
+		int playerCount = PhotonNetwork.countOfPlayersOnMaster;
+
+		if(refreshPlayerCountText)
+		{
+			playerCountText.GetComponent<TextMeshProUGUI>().text = "Players Online: " + playerCount;
+		}
+
+		yield return new WaitForSeconds(5);
+		StartCoroutine("RefreshPlayerCount");	
+	}
+
+	void InitUI()
+	{
+		mainCanvas.SetActive(true);
+		roomCreateCanvas.SetActive(false);
+		roomCanvas.SetActive(false);
 	}
 
 }
